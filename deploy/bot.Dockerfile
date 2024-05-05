4c83c0b37fb5edb1604abc4e564654476eeedcf6
hobbeat <hobbeat@mail.ru>
2024-05-05 20:11:05 +0300

    change deplloy

--- deploy/bot.Dockerfile
+++
@@ -1,3 +0,0 @@
-# FROM python:3.10.8-slim
-FROM pikabu_bot:latest
-CMD ["python", "pikabu_video_downloader_bot.py"]
--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
- docker compose down
- docker build . -t pikabu_bot
- docker compose build
- docker compose up -d
- docker ps
+#!/bin/bash
+DEPLOY_PATH="./deploy"
+DOCKER_FILE="${DEPLOY_PATH}/Dockerfile"
+COMPOSE_FILE="${DEPLOY_PATH}/docker-compose.yml"
+
+DB_PATH="./db"
+FILE_PATH="./files"
+
+docker compose -f $COMPOSE_FILE down
+mkdir $DB_PATH
+mkdir $FILE_PATH
+chmod 666 $DB_PATH
+chmod 666 $FILE_PATH
+docker build -t pvd_bot_image -f $DOCKER_FILE .
+docker compose -f $COMPOSE_FILE build
+docker compose -f $COMPOSE_FILE up -d
+docker ps
